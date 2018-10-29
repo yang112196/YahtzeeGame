552fe65b336091038762a3513d196e8dc608107d
yang112196 <yang112196@gmail.com>
2018-10-29 10:36:22 -0500

    Structure1

---
+++ Yahtzee/Assets/Scripts/Dice1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+    public int diceNum;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   
+    void dragDice(){}//Dice are able to be controlled by mouse
+    void acceptDice(){}//Accept dice from player's dragging
+}
