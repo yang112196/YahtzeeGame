ac354c9a0250d8a62f298c7dbc974ea8a46d7804
yang112196 <yang112196@gmail.com>
2018-11-11 23:58:26 -0600

    Dice
    Set up 5 dices and animation about dice rolling, then show the dice value

---
+++ Yahtzee/Assets/Scenes/Dice.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+
+    Rigidbody rb;
+
+    bool hasLanded;
+    bool thrown;
+
+    Vector3 initPosition;
+
+    public int diceValue;
+
+    public DiceSide[] diceSides;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        initPosition = transform.position;
+        rb.useGravity = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RollDice();
+        }
+
+        if(rb.IsSleeping() && !hasLanded && thrown)
+        {
+            hasLanded = true;
+            rb.useGravity = false;
+            rb.isKinematic = true;
+
+            SideValueCheck();
+        }
+        else if(rb.IsSleeping() && hasLanded && diceValue == 0)
+        {
+            RollAgain();
+        }
+    }
+
+    void RollDice()
+    {
+        if(!thrown && !hasLanded)
+        {
+            thrown = true;
+            rb.useGravity = true;
+            rb.AddTorque(Random.Range(0,500), Random.Range(0, 500), Random.Range(0, 500));
+        }
+        else if(thrown && hasLanded)
+        {
+            //When the dice landed but still throwing, then what do you want to do
+            Reset();
+        }
+    }
+
+    private void Reset()
+    {
+        transform.position = initPosition;
+        thrown = false;
+        hasLanded = false;
+        rb.useGravity = false;
+        rb.isKinematic = false;
+    }
+
+    void RollAgain()
+    {
+        Reset();
+        thrown = true;
+        rb.useGravity = true;
+        rb.AddTorque(Random.Range(0, 500), Random.Range(0, 500), Random.Range(0, 500));
+    }
+
+    void SideValueCheck()
+    {
+        diceValue = 0;
+        foreach(DiceSide side in diceSides)
+        {
+            if(side.OnGround())
+            {
+                diceValue = side.sideValue;
+                Debug.Log(diceValue + " has been rolled!");
+            }
+        }
+
+    }
+}
---
+++ Yahtzee/Assets/Scenes/DiceSide.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSide : MonoBehaviour {
+
+    bool onGround;
+    public int sideValue;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Ground")
+        {
+            onGround = false;
+        }
+
+    }
+
+    public bool OnGround()
+    {
+        return onGround;
+    }
+}
