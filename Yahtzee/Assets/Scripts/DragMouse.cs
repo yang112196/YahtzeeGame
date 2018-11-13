942d2b0d9883729a1fdf3f4340af87e47d0efee5
yang112196 <yang112196@gmail.com>
2018-11-12 22:09:36 -0600

    Dicedragging
    Now we can drag the dice to any position we want

---
+++ Yahtzee/Assets/Scripts/DragMouse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragMouse : MonoBehaviour {
+    float distance = 10;
+    private void OnMouseDrag()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        transform.position = new Vector3(objPos.x, transform.position.y, objPos.z);
+    }
+
+}
